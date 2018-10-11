cbae65b383ff9f4d67d106254463a9d969014528
alvintangzc <4008321@qq.com>
2018-10-11 22:47:35 +0800

    add web package

--- init-web.el
+++ init-web.el
@@ -1,2 +1,87 @@
 ;;; ~/.doom.d/init-web.el -*- lexical-binding: t; -*-
 
+(def-package! css-mode
+  :ensure nil
+  :init (setq css-indent-offset 2))
+
+;; SCSS mode
+(def-package! scss-mode
+  :init
+  ;; Disable complilation on save
+  (setq scss-compile-at-save nil))
+
+;; New `less-cs-mde' in Emacs 26
+(unless (fboundp 'less-css-mode)
+  (def-package! less-css-mode))
+
+;; CSS eldoc
+(def-package! css-eldoc
+  :commands turn-on-css-eldoc
+  :hook ((css-mode scss-mode less-css-mode) . turn-on-css-eldoc))
+
+;; JSON mode
+(def-package! json-mode)
+
+
+;; Improved JavaScript editing mode
+(def-package! js2-mode
+  :defines flycheck-javascript-eslint-executable
+  :mode (("\\.js\\'" . js2-mode)
+         ("\\.jsx\\'" . js2-jsx-mode))
+  :interpreter (("node" . js2-mode)
+                ("node" . js2-jsx-mode))
+  :hook ((js2-mode . js2-imenu-extras-mode)
+         (js2-mode . js2-highlight-unused-variables-mode))
+  :config
+  (with-eval-after-load 'flycheck
+    (if (or (executable-find "eslint_d")
+            (executable-find "eslint")
+            (executable-find "jshint"))
+        (setq js2-mode-show-strict-warnings nil))
+    (if (executable-find "eslint_d")
+        ;; https://github.com/mantoni/eslint_d.js
+        ;; npm -i -g eslint_d
+        (setq flycheck-javascript-eslint-executable "eslint_d")))
+
+  (def-package! js2-refactor
+    :diminish js2-refactor-mode
+    :hook (js2-mode . js2-refactor-mode)
+    :config (js2r-add-keybindings-with-prefix "C-c C-m")))
+
+
+;; Major mode for editing web templates
+(def-package! web-mode
+  :defines company-backends
+  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
+  :init
+    (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (add-hook 'web-mode-hook 'lsp-vue-enable)
+
+  ;; Complete for web,html,emmet,jade,slim modes
+  (def-package! company-web
+      :after company
+      :init (dolist (mode '(company-web-html company-web-jade company-web-slim))
+              (cl-pushnew mode company-backends))))
+
+;; Format HTML, CSS and JavaScript/JSON by js-beautify
+(def-package! web-beautify
+  :init
+  (with-eval-after-load 'js-mode
+    (bind-key "C-c b" #'web-beautify-js js-mode-map))
+  (with-eval-after-load 'js2-mode
+    (bind-key "C-c b" #'web-beautify-js js2-mode-map))
+  (with-eval-after-load 'json-mode
+    (bind-key "C-c b" #'web-beautify-js json-mode-map))
+  (with-eval-after-load 'web-mode
+    (bind-key "C-c b" #'web-beautify-html web-mode-map))
+  (with-eval-after-load 'sgml-mode
+    (bind-key "C-c b" #'web-beautify-html html-mode-map))
+  (with-eval-after-load 'css-mode
+    (bind-key "C-c b" #'web-beautify-css css-mode-map))
+  :config
+  ;; Set indent size to 2
+  (setq web-beautify-args '("-s" "2" "-f" "-")))
--- packages.el
+++ packages.el
@@ -9,8 +9,19 @@
 ;; common
 (package! symbol-overlay)
 
+;; language
+(package! css-mode)
+(package! scss-mode)
+(package! css-eldoc)
+(package! json-mode)
+(package! js2-mode)
+(package! js2-refactor)
+(package! web-mode)
+(package! web-beautify)
+
 ;; code
 (package! company)
+(package! company-web)
 (package! company-lsp)
 (package! lsp-mode)
 (package! lsp-ui)
